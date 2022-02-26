75ca463fcfa6cdaae262b69b7caeb54c88ab4843
fr4nkastro <85426216+fr4nkastro@users.noreply.github.com>
2022-02-25 22:27:22 -0600

    Eliminación de función detectionRectangle innecesaria

--- main.py
+++ main.py
@@ -112,18 +112,6 @@ def drawRectangle(thickness):
     cv2.waitKey()
     cv2.destroyAllWindows()
 
-#############################################################################################
-# def detectionRectangle():
-#     img = np.zeros((200, 200), dtype=np.uint8)
-#     img[50:150, 50:150] = 255
-#     ret, thresh = cv2.threshold(img, 127, 255, 0)
-#     image, contours, hierarchy = cv2.findContours(thresh, 
-#         cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-#     color = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
-#     img = cv2.drawContours(color, contours, -1, (0,255,0), 2)
-#     cv2.imshow("contours", color)
-#     cv2.waitKey()
-#     cv2.destroyAllWindows()
 
 #############################################################################################
 def detectionRectangleContours():
